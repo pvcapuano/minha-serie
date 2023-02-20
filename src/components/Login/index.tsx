418b5324dae999f27eeb91f6f186d86144bad95b
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-02-20 18:01:28 -0300

    feat: create login component/styles only

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="flex-1 bg-green-300 text-xs sm:text-sm flex flex-col justify-center items-center">
-      <h1 className="font-extrabold text-2xl sm:text-4xl">login</h1>
+    <div className="flex-1 text-xs sm:text-sm flex flex-col justify-center items-center gap-2 sm:gap-4">
+      <h1 className="font-extrabold select-none text-2xl sm:text-4xl">LOGIN</h1>
+      <input
+        type="text"
+        placeholder="Email"
+        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-500 text-slate-900 p-2 w-full max-w-[40ch]"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-500 text-slate-900 p-2 w-full max-w-[40ch]"
+      />
+      <button className="w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900">
+        <h2 className="relative z-20">Sign In</h2>
+      </button>
     </div>
   );
 };
