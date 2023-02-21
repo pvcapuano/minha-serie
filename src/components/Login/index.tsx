56608cff900968d3eaed326992f23d1e1c0b762b
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-02-21 12:04:22 -0300

    feat: add toastify library

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,23 +1,55 @@
-import React from "react";
+import Head from "next/head";
+import React, { useState, FormEvent } from "react";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp(event: FormEvent) {
+    event.preventDefault();
+
+    if (email === "" || password === "") {
+      toast.error("Preencha todos os campos.");
+      return;
+    }
+  }
+
   return (
-    <div className="flex-1 text-xs sm:text-sm flex flex-col justify-center items-center gap-2 sm:gap-4">
-      <h1 className="font-extrabold select-none text-2xl sm:text-4xl">LOGIN</h1>
-      <input
-        type="text"
-        placeholder="Email"
-        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-500 text-slate-900 p-2 w-full max-w-[40ch]"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-500 text-slate-900 p-2 w-full max-w-[40ch]"
-      />
-      <button className="w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900">
-        <h2 className="relative z-20">Sign In</h2>
-      </button>
-    </div>
+    <>
+      <Head>
+        <title>My Workout | Login</title>
+      </Head>
+      <form
+        className="flex-1 text-xs sm:text-sm flex flex-col justify-center items-center gap-2 sm:gap-4"
+        onSubmit={handleSignUp}
+      >
+        <h1 className="font-extrabold select-none text-2xl sm:text-4xl">
+          LOGIN
+        </h1>
+
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-500 text-slate-900 p-2 w-full max-w-[40ch]"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          className="outline-none duration-300 border-b-2 border-solid border-white focus:border-cyan-500 text-slate-900 p-2 w-full max-w-[40ch]"
+        />
+        <button
+          type="submit"
+          className="w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900"
+        >
+          <h2 className="relative z-20">Sign In</h2>
+        </button>
+      </form>
+    </>
   );
 };
 
